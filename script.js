e5356e1d764ab1a5c7db8e1b470c4054ab7df4a1
Cassio-Ares <cassiotadeu.ads@gmail.com>
2023-10-07 09:42:15 -0300

    bug de calculos

--- script.js
+++ script.js
@@ -7,24 +7,37 @@ function calculate() {
   const result_year = document.querySelector("#result_year");
   const result_months = document.querySelector("#result_months");
   const result_day = document.querySelector("#result_day");
+  const notice = document.querySelectorAll(".alert");
 
-  let dateNow= new Date( )
-
+  let dateNow = new Date();
+  let dataYear = new Date().getFullYear()
 
 
   if (day.value == "" || month.value == "" || year.value == "") {
-   
-  }else{
-    const datePass = new Date(day.value/month.value/year.value)
-    
-
-
-
-    console.log(dataDay)
-    
+     for(let i = 0; i < notice.length; i++ ){
+      notice[i].classList.add("alertVisi")
+     }
   }
+   else if(year.value > dataYear || month.value == 0 || month.value > 12){
+     for(let i = 0; i < notice.length; i++){
+      notice[i].classList.add("alertVisi")
+      notice[i].innerText = 'Must be a valid date'
+     }
+   }  
+  else {
+    const monthReal = month.value - 1;
 
-  
+    const datePass = new Date(year.value, monthReal, day.value);
+
+    const min = (dateNow - datePass) / 60000;
+
+    const yearTotal = Math.floor(min / 525960);
+    const monthTotal = Math.floor(min / 525960) % 43830; // ????
+    const dayTotal = (min/525960/43830)/1440             // ????
+
+    result_year.innerText = `${yearTotal}`;
+    result_months.innerText = `${monthTotal}`;
+  }
 }
 
 btn.addEventListener("click", calculate);
